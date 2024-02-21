f6c0471c9af5d8817ec53ee0ea4cff8d53db2daa
Alice K <61752564+akoening@users.noreply.github.com>
2024-02-21 19:56:52 +0000

    leftover from last week

--- src/pages/components/button.js
+++ src/pages/components/button.js
@@ -1,13 +1,12 @@
 export default function Button() {
-//  function handleClick() {
-//    document.getElementById('myBtn').style.backgroundColor = 'green';
-//    document.getElementById('myBtn').style.fontSize = '50px';
-//  }
+  function handleClick() {
+      document.getElementById('myBtn').style.backgroundColor = 'green';
+      document.getElementById('myBtn').style.fontSize = '50px';
+  }
   return (
-    <button id='myBtn' onClick={() => {
-        document.getElementById('myBtn').style.backgroundColor = 'green';
-        document.getElementById('myBtn').style.fontSize = '50px';
-    }}>
+    <button id='myBtn' onClick={(handleClick)}> 
+       // document.getElementById('myBtn').style.backgroundColor = 'green';
+        //document.getElementById('myBtn').style.fontSize = '50px';
       Click me
     </button>
   );
--- src/pages/components/queueUpdates.js
+++ src/pages/components/queueUpdates.js
@@ -7,9 +7,9 @@ export default function Counter() {
     <>
       <h1>{number}</h1>
       <button onClick={() => {
-        setNumber(n => n + 1);
-        setNumber(n => n + 1);
-        setNumber(n => n + 1);
+        setNumber(n => n + 1); // n = 0; setNumber(0)
+        setNumber(n => n + 1); // n = 1; setNumber(1)
+        setNumber(n => n + 1); // n = 2; setNumber(2)
       }}>+3</button>
     </>
   )
